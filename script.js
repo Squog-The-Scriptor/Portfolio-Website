a857c3cc9566bfa68970f48ebb98c895a9465756
Kevin Long <kevinlong1234@gmail.com>
2023-08-15 00:10:18 -0400

    Added drop down menu to the right side of the navbar with links to the gospel and hope markham
    Adjusted styling margins for cooking image in the about section

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Drop Down Bar Event Click Functions */
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.drop-button')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+
+
+
+  
